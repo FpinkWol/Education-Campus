f244c0666afd556c81021d41398792ff922c81e7
AMIRHUSAINZAREI <ahzarei78@gmail.com>
2021-02-21 15:12:56 +0330

    add some premission to views

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse, JsonResponse
 
-from rest_framework.parsers import JSONParser
 from rest_framework import status
 from rest_framework import generics
 from rest_framework.views import APIView
+from rest_framework.parsers import JSONParser
+from rest_framework.permissions import (IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly,
+                                        AllowAny,
+                                        BasePermission,
+                                        SAFE_METHODS)
 
 from .serializers import *
 from .models import Teacher, Video
@@ -13,7 +18,23 @@ from .models import Teacher, Video
         # API #
         #######
 
+class IsProfileOwnerOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user == view.get_object()
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
+
+
+
 class TeacherProfileView(generics.ListAPIView):
+
+    permission_classes = [IsAuthenticated]
     
     queryset = Teacher.objects.all()
     serializer_class = TeacherProfileSerializer
@@ -21,13 +42,17 @@ class TeacherProfileView(generics.ListAPIView):
 
    
 class DetailTeacherProfileView(generics.RetrieveUpdateDestroyAPIView):
-    
+
+    permission_classes = [IsAuthenticated, IsProfileOwnerOrReadOnly]
+
     queryset = Teacher.objects.all()
     serializer_class = TeacherProfileSerializer
 
 
 
 class StudentProfileView(generics.ListAPIView):
+
+    permission_classes = [IsAuthenticated]
 
     queryset = Student.objects.all()
     serializer_class = StudentProfileSerializer
@@ -36,19 +61,42 @@ class StudentProfileView(generics.ListAPIView):
 
 class DetailStudentProfileView(generics.RetrieveUpdateDestroyAPIView):
 
+    permission_classes = [IsAuthenticated, IsProfileOwnerOrReadOnly]
+
     queryset = Student.objects.all()
     serializer_class = StudentProfileSerializer
 
 
 
+class IsTeacherOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        else:
+            return isinstance(view.get_object().user, Teacher)
+
+
+
 class CourseView(generics.ListCreateAPIView):
     
+    permission_classes = [IsAuthenticated, IsTeacherOrReadOnly]
+
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
 
+class IsCourseOwnerOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user == view.get_object().teacher
+
 
 class DetailCourseView(generics.RetrieveUpdateDestroyAPIView):
+
+    permission_classes = [IsAuthenticated, IsTeacherOrReadOnly, IsCourseOwnerOrReadOnly]
     
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
@@ -57,12 +105,16 @@ class DetailCourseView(generics.RetrieveUpdateDestroyAPIView):
 
 class ArticleView(generics.ListCreateAPIView):
 
+    permission_classes = [IsAuthenticated, IsTeacherOrReadOnly]
+
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
 
 
 class DetailArticleView(generics.RetrieveUpdateDestroyAPIView):
+
+    permission_classes = [IsAuthenticated, IsTeacherOrReadOnly]
 
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
@@ -78,6 +130,7 @@ class VideoView(generics.ListCreateAPIView):
 
 class CourseVideoView(generics.ListAPIView):
 
+    serializer_class = [IsAuthenticated]
     serializer_class = VideoSerializer
 
     def get_queryset(self):
